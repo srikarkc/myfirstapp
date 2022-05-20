b97e7d3a44e3badd01b36b40357373e02fa0b098
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2022-05-20 14:47:32 -0700

    Update WelcomeController.java

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
  @GetMapping("/welcome")
 	public String welcome() {
-	 return "Hello World!";
+	 return "Hello World! Made by KC.";
  }
 }
